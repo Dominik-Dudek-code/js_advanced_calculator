c35d5c11a5947423d45331b5336696eb1c76e2f6
Dominik Dudek <dom-dud774@o2.pl>
2024-10-14 01:01:47 +0200

    Task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,68 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(value) {
+      this.result += value;
+
+      return this;
+    },
+
+    subtract(value) {
+      this.result -= value;
+
+      return this;
+    },
+
+    multiply(value) {
+      this.result *= value;
+
+      return this;
+    },
+
+    divide(value) {
+      if (value !== 0) {
+        this.result /= value;
+      } else {
+        throw new Error("Can't divide by 0!");
+      }
+
+      return this;
+    },
+
+    operate(operation, value) {
+      if (value === undefined) {
+        throw new Error('No value given');
+      }
+
+      switch (operation) {
+        case this.add:
+          this.add(value);
+          break;
+        case this.subtract:
+          this.subtract(value);
+          break;
+        case this.multiply:
+          this.multiply(value);
+          break;
+        case this.divide:
+          this.divide(value);
+          break;
+        default:
+          throw new Error('Unknown operation');
+      }
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
